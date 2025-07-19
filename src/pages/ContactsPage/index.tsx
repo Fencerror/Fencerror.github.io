974a1f7f628ed67ccffda2bdc43297bb1d7aa38d
Fencerror <tyghrtzwrcv@gmail.com>
2025-07-19 19:25:34 +0700

    fix errors....

--- src/pages/ContactsPage/index.tsx
+++ src/pages/ContactsPage/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Navigation } from '../../widgets/nav';
 import '../../app/styles/fonts.css';
-import './style.css'; 
+
 export const Contacts: React.FC = () => {
   return (
     <>
-      <section id="contact"> 
+      <section id="contact" className="flex items-center justify-center h-screen w-screen">
         <div className="text-center text-white">
-          <h2 className="text-2xl  font-400 ">Свободно пишите на одну из платформ:</h2>
+          <h2 className="text-2xl font-normal">Свободно пишите на одну из платформ:</h2>
           <ul className="mt-6 space-y-4">
             <li className="text-lg">
-              Telegram: <a href="https://t.me/fencerror" target="_blank" rel="noopener noreferrer" className="text-blue-500">@fencerror</a>
+              Telegram: <a href="https://t.me/fencerror" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">@fencerror</a>
             </li>
             <li className="text-lg">
-              VK: <a href="https://vk.com/stepan_orlow" target="_blank" rel="noopener noreferrer" className="text-blue-500">vk.com/stepan_orlow</a>
+              VK: <a href="https://vk.com/stepan_orlow" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">vk.com/stepan_orlow</a>
             </li>
             <li className="text-lg">
-              Email: <a href="mailto:tyghrtzwrcv@gmail.com" className="text-blue-500">tyghrtzwrcv@gmail.com</a>
+              Email: <a href="mailto:tyghrtzwrcv@gmail.com" className="text-blue-500 hover:underline">tyghrtzwrcv@gmail.com</a>
             </li>
           </ul>
         </div>
@@ -25,4 +25,3 @@ export const Contacts: React.FC = () => {
     </>
   );
 };
-
